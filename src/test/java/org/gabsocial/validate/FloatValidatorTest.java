afde854e7113cfda980f3109a9227a7e33725062
sysdevone <sysdevone@users.noreply.github.com>
2016-04-06 00:04:33 -0700

    changed package to validate from gabvalidate

---
+++ src/test/java/org/gabsocial/validate/FloatValidatorTest.java
@@ -0,0 +1,109 @@
+/*****************************************************************************************
+ *
+ * Copyright 2015 Gregory Brown. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *****************************************************************************************
+ */
+
+package org.gabsocial.validate;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * A test class for the FloatValidator
+ *
+ * @author Gregory Brown (sysdevone)
+ *
+ */
+public class FloatValidatorTest
+{
+    @Before
+    public void setUp()
+    {
+        //
+    }
+    
+    @After
+    public void tearDown()
+    {
+        
+    }
+    
+    @Test
+    public void testMaxValue()
+    {
+
+        try
+        {
+            float x = 5;
+            float max = 10;
+            boolean retVal = Validate.defineFloat(x).testMaxValue(max)
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+    
+    @Test
+    public void testMinLength()
+    {
+        
+        try
+        {
+            float x = 5;
+            float min = 4;
+            boolean retVal = Validate.defineFloat(x).testMinValue(min)
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+        
+    }
+    
+    
+    @Test
+    public void testEquals()
+    {
+        
+        try
+        {
+            float x = 5;
+            float y = 5;
+            boolean retVal = Validate.defineFloat(x).testEquals(y)
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+        
+    }
+    
+}
---
+++ src/test/java/org/gabsocial/validate/StringValidatorTest.java
@@ -0,0 +1,193 @@
+/*****************************************************************************************
+ *
+ * Copyright 2015 Gregory Brown. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *****************************************************************************************
+ */
+
+package org.gabsocial.validate;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * A test class for the StringValidator
+ *
+ * @author Gregory Brown (sysdevone)
+ *
+ */
+public class StringValidatorTest
+{
+    @Before
+    public void setUp()
+    {
+        //
+    }
+
+    @After
+    public void tearDown()
+    {
+
+    }
+    
+    @Test
+    public void testMaxLength()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testMaxLength(10)
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+
+    @Test
+    public void testMinLength()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testMinLength(8)
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+
+    @Test
+    public void testNotNullEmpty()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testNotNullEmpty()
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+    
+
+    @Test
+    public void testNotNull()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testNotNull()
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+    
+    @Test
+    public void testEquals()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testEquals("HelloWorld")
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+    
+    @Test
+    public void testEqualsNoCase()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testEqualsNoCase("hELLOwORLD")
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+    
+    @Test
+    public void testMatch()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testMatch("HelloWorld")
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+    
+    @Test
+    public void testMatch2()
+    {
+
+        try
+        {
+            boolean retVal = Validate.defineString("HelloWorld").testMatch("[A-Za-z]*")
+                    .throwExceptionOnFailedValidation().validate();
+
+            Assert.assertTrue(retVal);
+        }
+        catch (final ValidateException e)
+        {
+            Assert.fail(e.toString());
+        }
+
+    }
+        
+}
